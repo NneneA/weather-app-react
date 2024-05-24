08f0dfc4d18761cb508f5be1b72532a43b92223e
NneneA <nne.akuma@gmail.com>
2024-05-24 16:31:16 +0100

    Updated the Search.js

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import './App.css';
+import "./styles.css";
 
 export default function Search() {
     let [city, setCity] = useState(null);
