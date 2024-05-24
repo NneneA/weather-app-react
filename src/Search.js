0b004fceec5a7535bd7e05e29bb04d20fdb17e12
NneneA <nne.akuma@gmail.com>
2024-05-24 16:51:10 +0100

    updated the index and Search.js

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./styles.css";
+import "./search.css";
 
 export default function Search() {
     let [city, setCity] = useState(null);
