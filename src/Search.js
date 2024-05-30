16a33dc32b20dcb6c149231860845b8cfa235dad
NneneA <nne.akuma@gmail.com>
2024-05-30 14:46:07 +0100

    Updated the Search.js file

--- src/Search.js
+++ src/Search.js
@@ -3,58 +3,62 @@ import axios from "axios";
 import "./search.css";
 
 export default function Search() {
-    let [city, setCity] = useState(null);
-  let [output, setOutput] = useState("");
-  let [temperature, setTemperature] = useState(null);
-  let [description, setDescription] = useState(null);
-  let [humidity, setHumidity] = useState(null);
-  let [wind, setWind] = useState(null);
-  let [icon, setIcon] = useState(null);
+  const [city, setCity] = useState(null);
+  const [output, setOutput] = useState("");
+  const [temperature, setTemperature] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [humidity, setHumidity] = useState(null);
+  const [wind, setWind] = useState(null);
+  const [icon, setIcon] = useState(null);
 
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=72bb9dab46b9ec3d65f423c63f27a9b8&units=metric`;
-    axios.get(url).then(showWeather);
-    console.log(url);
-    function showWeather(response) {
-      setTemperature(response.data.main.temp);
-      setDescription(response.data.weather[0].description);
-      setHumidity(response.data.main.humidity);
-      setWind(response.data.wind.speed);
-      setIcon(
-        `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-      );
-    }
-
+  function showWeather(response) {
+    setTemperature(response.data.main.temp);
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWind(response.data.wind.speed);
+    setIcon(
+      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
     setOutput(
       <div className="Output">
         <h2>{city}</h2>
-        <p>Temperature: {Math.round(temperature)}°C</p>
-        <p>Description: {description}</p>
-        <p>Humidity: {humidity}%</p>
-        <p>Wind: {wind}km/h</p>
+        <p>Temperature: {Math.round(response.data.main.temp)}°C</p>
+        <p>Description: {response.data.weather[0].description}</p>
+        <p>Humidity: {response.data.main.humidity}%</p>
+        <p>Wind: {response.data.wind.speed}km/h</p>
         <p>
-          Icon: <img src={icon} alt={description} />
+          Icon:{" "}
+          <img
+            src={`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
+            alt={response.data.weather[0].description}
+          />
         </p>
       </div>
     );
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=72bb9dab46b9ec3d65f423c63f27a9b8&units=metric`;
+    axios.get(url).then(showWeather);
+  }
+
   function updateCity(event) {
     setCity(event.target.value);
   }
-    return (
-        <div className="App">
-          <form onSubmit={handleSubmit}>
-            <input
-              type="search"
-              placeholder="Type a city.."
-              onChange={updateCity}
-            />
-            <button type="submit">Search</button>
-          </form>
-          <div>{output}</div>
-        </div>
-      );
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Type a city.."
+          onChange={updateCity}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div>{output}</div>
+    </div>
+  );
 }
