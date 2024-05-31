60e6e22cf7fbc50eb32f88f339000d3e9f0746f8
NneneA <nne.akuma@gmail.com>
2024-05-31 17:15:25 +0100

    updated the search.js

--- src/Search.js
+++ src/Search.js
@@ -4,14 +4,16 @@ import "./search.css";
 
 export default function Search() {
   const [city, setCity] = useState(null);
-  const [output, setOutput] = useState("");
+  const [loaded, setLoaded] = useState(false);
   const [temperature, setTemperature] = useState(null);
   const [description, setDescription] = useState(null);
   const [humidity, setHumidity] = useState(null);
   const [wind, setWind] = useState(null);
   const [icon, setIcon] = useState(null);
+  const [cityName, setCityName] = useState(null);
 
   function showWeather(response) {
+    setLoaded(true);
     setTemperature(response.data.main.temp);
     setDescription(response.data.weather[0].description);
     setHumidity(response.data.main.humidity);
@@ -19,28 +21,13 @@ export default function Search() {
     setIcon(
       `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
-    setOutput(
-      <div className="Output">
-        <h2>{city}</h2>
-        <p>Temperature: {Math.round(response.data.main.temp)}°C</p>
-        <p>Description: {response.data.weather[0].description}</p>
-        <p>Humidity: {response.data.main.humidity}%</p>
-        <p>Wind: {response.data.wind.speed}km/h</p>
-        <p>
-          Icon:{" "}
-          <img
-            src={`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
-            alt={response.data.weather[0].description}
-          />
-        </p>
-      </div>
-    );
+    setCityName(response.data.name);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=72bb9dab46b9ec3d65f423c63f27a9b8&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=72bb9dab46b9ec3d65f423c63f27a9b8&units=metric;`;
     axios.get(url).then(showWeather);
   }
 
@@ -58,7 +45,18 @@ export default function Search() {
         />
         <button type="submit">Search</button>
       </form>
-      <div>{output}</div>
+      {loaded && (
+        <div className="Output">
+          <h2>{cityName}</h2>
+          <p>Temperature: {Math.round(temperature)}°C</p>
+          <p>Description: {description}</p>
+          <p>Humidity: {humidity}%</p>
+          <p>Wind: {wind}km/h</p>
+          <p>
+            Icon: <img src={icon} alt={description} />
+          </p>
+        </div>
+      )}
     </div>
   );
 }
